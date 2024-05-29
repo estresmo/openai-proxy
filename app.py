047a44ce0413f0c0c1703462967bc0b146b0d1ce
juan <juansuarez2710@gmail.com>
2024-05-28 22:50:02 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import requests
+import urllib.parse
+
+app = Flask(__name__)
+@app.route('/<path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'PATCH'])
+def general(path):
+    print(request)
+    headers = request.headers
+    BASE_URL = 'https://api.openai.com/v1/'
+    new_url = urllib.parse.urljoin(BASE_URL, path)
+    resp = requests.request(request.method, new_url, headers=headers, data=request.data)
+    return  resp.content, resp.status_code, resp.headers.items()
+
+
+if __name__ == '__main__':
+    app.run(port=4999, debug=True)
